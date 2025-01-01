d7601f996ad8763e4154780b6f097a6df0f12245
Jay <jay@Jays-Mac.local>
2025-01-01 17:37:13 +0530

    1 jan p1

---
+++ BinarySearch/SearchElemINrotatedarrsorted/duplicates/brute linear/main.cpp
@@ -0,0 +1,8 @@
+bool searchInARotatedSortedArrayII(vector<int>&A, int key) {
+    // Write your code here.
+    for(int i = 0 ; i < A.size() ; i++){
+        if(A[i] == key) return true;
+    }
+        return false;
+}
+tc =o (n)
