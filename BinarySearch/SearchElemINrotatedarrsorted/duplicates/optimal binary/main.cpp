91c2400ef42c51cd756f4f4571349233748c50f9
Jay <jay@Jays-Mac.local>
2025-01-02 12:27:42 +0530

    2 jan 2

--- BinarySearch/SearchElemINrotatedarrsorted/duplicates/optimal binary/main.cpp
+++ BinarySearch/SearchElemINrotatedarrsorted/duplicates/optimal binary/main.cpp
@@ -1,42 +1,40 @@
-bool searchInARotatedSortedArrayII(vector<int>&A, int key) {
-    // Write your code here.
-    int n = A.size();
-    int low = 0; 
-    int high = n - 1;
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) return true;
 
-    while(low <= high){
-        int mid = low + (high - low) / 2;
-
-        if(A[mid] == key) return true;
-
-        // duplicate case  low = mid = high cant determine which portion is sorted
-
-        if(A[low] == A[mid] && A[mid] == A[high]){
-            low = low + 1;
-            high = high - 1;
-        }
-
-        // left sorted
-        if(A[low] <= A[mid]){
-            if(A[low] <= key && key <= A[mid]){
-                high = mid - 1;
+            // duplicate case 
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
             }
+            // left sorted
+            else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            // right sorted
             else{
-                low = mid + 1;
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
-        // right sorted
-        if (A[mid] <= A[high]) {
-            if(A[mid] <= key && key <= A[high]){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-        }
+        return false;
     }
-    return false;
-}
+};
 TC = O(logn) base2
 
 test case = where many duplicates we will end up shrinking the array a lot so o(n/2)
