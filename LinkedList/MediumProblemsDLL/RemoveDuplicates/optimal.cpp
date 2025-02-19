20c786cbdfd629988ac755ec4a8662b9ef17786f
JayTheCoder77 <jayantiyer90@gmail.com>
2025-02-19 15:57:36 +0530

    19 feb 2025

---
+++ LinkedList/MediumProblemsDLL/RemoveDuplicates/optimal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition of doubly linked list:
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next, Node *prev) {
+ *          this->data = data;
+ *          this->prev = prev;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+ Node * removeDuplicates(Node *head)
+ {
+     // Write your code here
+     Node* temp = head;
+     while(temp != NULL && temp -> next != head){
+         Node* nextNode = temp -> next;
+         while(nextNode != NULL && nextNode -> data == temp -> data){
+             Node* duplicate = nextNode;
+             nextNode = nextNode -> next;
+             free(duplicate);
+         }
+         temp -> next = nextNode;
+         if(nextNode) nextNode -> prev = temp;
+         temp = nextNode;
+     }
+     return head;
+ }
+tc = o(n) // no its not o(n2)
+sc = o(1) 
