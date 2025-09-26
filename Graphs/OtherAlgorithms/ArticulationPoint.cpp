5f221e56b14086d16e87ce52e50e7dfb9b6f1b7e
JayTheCoder77 <jayantiyer90@gmail.com>
2025-09-26 15:10:04 +0530

    26 sep 2025

---
+++ Graphs/OtherAlgorithms/ArticulationPoint.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  private:
+    int timer = 0;
+    
+    void dfs(int node , int parent , vector<int> adj[] , int tin[] , int low[] , vector<int> &mark , vector<int> &vis){
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for(auto it : adj[node]){
+            if(it == parent) continue;
+            if(!vis[it]){
+               dfs(it , node , adj , tin , low , mark , vis);
+               low[node] = min(low[node] , low[it]);
+               
+               // articulation point
+               if(low[it] >= tin[node] && parent != -1) mark[node] = 1;
+               child++;
+            }
+            else{
+                low[node] = min(low[node] , tin[it]);
+            }
+        }
+        if(child > 1 && parent == -1) mark[node] = 1;
+    }
+  public:
+    vector<int> articulationPoints(int V, vector<int> adj[]) {
+        vector<int> vis(V , 0);
+        int tin[V];
+        int low[V]; // lowest time of insertion excluding parent and visited
+        vector<int> mark(V , 0);
+        for(int i = 0 ; i < V ; i++){
+            if(!vis[i]) dfs(i , - 1 , adj , tin , low , mark , vis);
+        }
+        vector<int> ans;
+        for(int i = 0 ; i < V ; i++){
+            if(mark[i] == 1) ans.push_back(i);
+        }
+        if(ans.empty()) return {-1}; 
+        return ans;
+    }
+};
